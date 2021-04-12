277cedd75b337d7abf7ea904492470cd933be99b
KhaledTaymour <khaledtaymour@gmail.com>
2021-04-12 12:56:41 +0200

    [feat]: update lists based on BE

--- src/components/bookList/BookList.js
+++ src/components/bookList/BookList.js
@@ -2,13 +2,21 @@ import React from "react";
 import BookListContent from "../bookListContent/BookListContent";
 import { Link } from "react-router-dom";
 
-const BookList = () => {
+const BookList = ({
+  book_records_currently,
+  book_records_want,
+  book_records_read,
+}) => {
   return (
     <div className="list-books">
       <div className="list-books-title">
         <h1>MyReads</h1>
       </div>
-      <BookListContent />
+      <BookListContent
+        book_records_currently={book_records_currently}
+        book_records_want={book_records_want}
+        book_records_read={book_records_read}
+      />
       <div className="open-search">
         <Link to="/search">
           <button>Add a book</button>
